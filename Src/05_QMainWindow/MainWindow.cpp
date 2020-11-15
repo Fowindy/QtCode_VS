1d1f04d7850e06a05159a11148cdd7292f197fa5
Fowindy <47002339@qq.com>
2020-11-15 11:33:55 +0800

    //连接[新建]动作=:可以访问变量;mutable:可以对变量进行操作

--- Src/05_QMainWindow/MainWindow.cpp
+++ Src/05_QMainWindow/MainWindow.cpp
@@ -15,4 +15,14 @@ MainWindow::MainWindow(QWidget *parent)
 
 	//添加[新建]子菜单项动作
 	QAction *pNew = pFile->addAction("新建");	//返回的是动作的指针
+	//定义按下次数变量
+	int num = 0;
+
+	//连接[新建]动作
+	connect(pNew, &QAction::triggered,
+		[=]()mutable	//=:可以访问变量;mutable:可以对变量进行操作
+	{
+		qDebug() << "新建被按下" << ++num;
+	}
+	);
 }
