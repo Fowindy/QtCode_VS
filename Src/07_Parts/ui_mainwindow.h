77d391996a00fc11b09f2153074d213c610e705d
Fowindy <47002339@qq.com>
2020-11-16 21:29:14 +0800

    ui完成菜单目录

--- Src/07_Parts/ui_mainwindow.h
+++ Src/07_Parts/ui_mainwindow.h
@@ -10,8 +10,10 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
@@ -22,9 +24,14 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QAction *actionnew;
+    QAction *actionbaocun;
     QWidget *centralwidget;
     QPushButton *button;
     QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menuEdit;
+    QMenu *menu_2;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -32,6 +39,10 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(800, 600);
+        actionnew = new QAction(MainWindow);
+        actionnew->setObjectName(QString::fromUtf8("actionnew"));
+        actionbaocun = new QAction(MainWindow);
+        actionbaocun->setObjectName(QString::fromUtf8("actionbaocun"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         button = new QPushButton(centralwidget);
@@ -41,10 +52,24 @@ public:
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 800, 23));
+        menu = new QMenu(menubar);
+        menu->setObjectName(QString::fromUtf8("menu"));
+        menuEdit = new QMenu(menubar);
+        menuEdit->setObjectName(QString::fromUtf8("menuEdit"));
+        menu_2 = new QMenu(menubar);
+        menu_2->setObjectName(QString::fromUtf8("menu_2"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menuEdit->menuAction());
+        menubar->addAction(menu_2->menuAction());
+        menu->addAction(actionnew);
+        menu->addSeparator();
+        menu->addAction(actionbaocun);
+        menu->addSeparator();
 
         retranslateUi(MainWindow);
         QObject::connect(button, SIGNAL(clicked()), MainWindow, SLOT(close()));
@@ -55,7 +80,12 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionnew->setText(QCoreApplication::translate("MainWindow", "\346\226\260\345\273\272", nullptr));
+        actionbaocun->setText(QCoreApplication::translate("MainWindow", "\344\277\235\345\255\230", nullptr));
         button->setText(QCoreApplication::translate("MainWindow", "button", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\346\226\207\344\273\266", nullptr));
+        menuEdit->setTitle(QCoreApplication::translate("MainWindow", "Edit", nullptr));
+        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\347\252\227\344\275\223", nullptr));
     } // retranslateUi
 
 };
