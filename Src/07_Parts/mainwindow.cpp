3baedea1b824aa4d4ecd8b3295c77bec5d453f3f
Fowindy <47002339@qq.com>
2020-11-16 10:00:39 +0800

    07_Parts_项目创建

---
+++ Src/07_Parts/mainwindow.cpp
@@ -0,0 +1,16 @@
+﻿#pragma execution_character_set("utf-8")
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
