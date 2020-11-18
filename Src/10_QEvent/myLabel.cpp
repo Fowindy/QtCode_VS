1bf194fe517935f2bb8bd33b4cacf504f0512866
Fowindy <47002339@qq.com>
2020-11-19 00:32:55 +0800

    创建自定义myLabel类

---
+++ Src/10_QEvent/myLabel.cpp
@@ -0,0 +1,10 @@
+#include "myLabel.h"
+
+myLabel::myLabel(QWidget *parent)
+	: QWidget(parent)
+{
+}
+
+myLabel::~myLabel()
+{
+}
---
+++ Src/10_QEvent/myLabel.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <QWidget>
+
+class myLabel : public QWidget
+{
+	Q_OBJECT
+
+public:
+	myLabel(QWidget *parent);
+	~myLabel();
+signals:
+public slots:
+};
