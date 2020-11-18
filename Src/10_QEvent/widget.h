8adfb24761dcf2fa6c82e4a1a71291d8a38ba8fb
Fowindy <47002339@qq.com>
2020-11-19 04:26:13 +0800

    [重点]传引用的方式同步两个计时器运行

--- Src/10_QEvent/widget.h
+++ Src/10_QEvent/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include "myLabel.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -19,8 +20,14 @@ protected:
 	void keyPressEvent(QKeyEvent *);	//占位符参数，方便以后修改函数定义且不需要修改接口
 	//计时器事件
 	void timerEvent(QTimerEvent *);
+
+	void TimerMethod(int &sec, QString timerType, int timerId, myLabel* label);
+
 private:
 	Ui::Widget *ui;
 	int timerId;	//定义定时器的Id
+	int timerId2;	//定义定时器Id2
+	int sec1;
+	int sec2;
 };
 #endif // WIDGET_H
