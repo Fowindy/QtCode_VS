73250ab8d9cbd055a17b4404a1c1370c29d8e5f0
Fowindy <47002339@qq.com>
2020-11-18 20:44:46 +0800

    颜色的三种表示方式

---
+++ Src/09_StyleSheet/widget.cpp
@@ -0,0 +1,25 @@
+﻿#pragma execution_character_set("utf-8")
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+	//颜色的三种表示方式:
+	/**
+	 * 1.rgb(0,220,235);
+	 * 2.#9013fe
+	 * 3.red
+	 * 4.background = background-color
+	 */
+	ui->label->setStyleSheet("QLabel{color:rgb(0,220,235);"
+		"background-color:#9013fe;}");
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
