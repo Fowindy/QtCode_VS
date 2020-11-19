7a8be1c88359e3921b7a18c47bc2b6272fdfd710
Fowindy <47002339@qq.com>
2020-11-19 16:45:28 +0800

    重写绘图事件,虚函数

---
+++ Src/11_PaintEvent/widget.cpp
@@ -0,0 +1,20 @@
+﻿#pragma execution_character_set("utf-8")
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
+void Widget::paintEvent(QPaintEvent *event)
+{
+}
+
