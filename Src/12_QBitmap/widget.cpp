5ffdee672fc207356a56bcd8b85f8c3336e33853
Fowindy <47002339@qq.com>
2020-11-20 08:33:01 +0800

    QPixmap_图片背景透明_彩色

---
+++ Src/12_QBitmap/widget.cpp
@@ -0,0 +1,27 @@
+﻿#pragma execution_character_set("utf-8")
+#include "widget.h"
+#include "ui_widget.h"
+#include <QPainter>
+#include <QPixmap>
+#include <QBitmap>
+
+Widget::Widget(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
+
+void Widget::paintEvent(QPaintEvent *)
+{
+	//定义画家对象
+	QPainter p(this);
+	//QPixmap_图片背景透明_彩色
+	p.drawPixmap(0, 0, QPixmap("../image/butterfly_tm.png"));
+}
+
---
+++ Src/12_QBitmap/widget.h
@@ -0,0 +1,25 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	Widget(QWidget *parent = nullptr);
+	~Widget();
+protected:
+	//重写绘图事件
+	//如果给窗口绘图,一定要在paintEvent()绘图
+	void paintEvent(QPaintEvent *);
+
+private:
+	Ui::Widget *ui;
+};
+#endif // WIDGET_H
