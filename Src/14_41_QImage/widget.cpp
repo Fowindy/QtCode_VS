73e68d75f2fe6ac830e7f5908c483854a3170939
Fowindy <47002339@qq.com>
2020-11-21 18:41:58 +0800

    QImage绘制透明图片完成

--- Src/14_41_QImage/widget.cpp
+++ Src/14_41_QImage/widget.cpp
@@ -1,12 +1,23 @@
 ﻿#pragma execution_character_set("utf-8")
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPainter>
+#include <QImage>
 
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
 	, ui(new Ui::Widget)
 {
 	ui->setupUi(this);
+	//创建一个绘图设备,QImage::Format_ARGB32背景透明
+	QImage image(400, 300, QImage::Format_ARGB32);
+	QPainter p;
+	p.begin(&image);
+	//绘图
+	p.drawImage(0, 0, QImage(":/image/face.png"));
+	p.end();
+	//绘图设备保存图片
+	image.save("./image/image.png");
 }
 
 Widget::~Widget()
