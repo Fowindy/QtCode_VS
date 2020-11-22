1c300c2c07bdb69efe893d5b3e723b9a9575468d
Fowindy <47002339@qq.com>
2020-11-22 11:29:08 +0800

    画图

--- Src/15_42_QPicture/widget.cpp
+++ Src/15_42_QPicture/widget.cpp
@@ -15,7 +15,13 @@ Widget::Widget(QWidget *parent)
 	QPicture picture;
 	//把画具给画家
 	painter.begin(&picture);
+	//画笑脸图片
+	painter.drawPixmap(0, 0, 80, 80, QPixmap(":/image/face.png"));
+	//画一条横向直线
+	painter.drawLine(50, 50, 150, 50);
 	painter.end();
+	//画好后保存
+	picture.save("./image/picture.png");
 }
 
 Widget::~Widget()
