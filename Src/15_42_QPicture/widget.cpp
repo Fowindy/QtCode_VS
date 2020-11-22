6fa10c7adc97ea7e5adbc9c64360f4b2222250bf
Fowindy <47002339@qq.com>
2020-11-22 11:21:31 +0800

    1.先创建画家对象

--- Src/15_42_QPicture/widget.cpp
+++ Src/15_42_QPicture/widget.cpp
@@ -1,12 +1,15 @@
 ﻿#pragma execution_character_set("utf-8")
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPainter>
 
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
 	, ui(new Ui::Widget)
 {
 	ui->setupUi(this);
+	//1.先创建画家对象
+	QPainter painter;
 }
 
 Widget::~Widget()
