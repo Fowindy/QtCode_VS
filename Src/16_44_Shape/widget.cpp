1aa9f2fc87db03209ea2f8871ee41402ae6983ba
Fowindy <47002339@qq.com>
2020-11-23 02:05:45 +0800

    [重点]---背景透明轮廓移动的坐标转换

--- Src/16_44_Shape/widget.cpp
+++ Src/16_44_Shape/widget.cpp
@@ -3,6 +3,8 @@
 #include "ui_widget.h"
 #include <QPainter>
 #include <QPixmap>
+#include <QMouseEvent>
+#include <QDebug>
 
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
@@ -12,7 +14,7 @@ Widget::Widget(QWidget *parent)
 	//setWindowFlags去widget窗口边框_Qt::FramelessWindowHint_加内联函数windowFlags
 	setWindowFlags(Qt::FramelessWindowHint | windowFlags());
 	//设定widget窗口背景透明_Qt::WA_TranslucentBackground
-	setAttribute(Qt::WA_TranslucentBackground);
+	setAttribute(Qt::WA_TranslucentBackground);	//透明背景将不是自身部分
 }
 
 Widget::~Widget()
@@ -26,4 +28,32 @@ void Widget::paintEvent(QPaintEvent *event)
 	QPainter painter(this);
 	//画家画画
 	painter.drawPixmap(0, 0, QPixmap(":/image/sunny.png"));
+}
+
+void Widget::mousePressEvent(QMouseEvent *event)
+{
+	//判断鼠标左右按键_右键关闭程序_左键坐标转换
+	if (event->button() == Qt::RightButton)
+	{
+		//右键关闭
+		close();
+	}
+	else if (event->button() == Qt::LeftButton)
+	{
+		//左键坐标转换_求坐标差值
+		//当前光标点-窗口左上角坐标
+		point = event->globalPos();//619,397(当前光标点)
+		point = this->frameGeometry().topLeft();//560,220(原始左上坐标)
+		point = event->globalPos() - this->frameGeometry().topLeft();//59,177(固定偏移点)
+		qDebug() << event->globalPos();
+	}
+}
+
+void Widget::mouseMoveEvent(QMouseEvent *event)
+{
+	if (event->buttons() & Qt::LeftButton)
+	{
+		point1 = event->globalPos();//新的光标点(880,560)
+		move(event->globalPos() - point);//新的左上角(880,560) - (59,177) = (560,220) + [(880,560) - (619,397)] 
+	}
 }
--- Src/16_44_Shape/widget.h
+++ Src/16_44_Shape/widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QPoint>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,8 +18,14 @@ public:
 
 protected:
 	void paintEvent(QPaintEvent *event);
+	//鼠标按下_按下鼠标坐标与窗口左上角坐标转换
+	void mousePressEvent(QMouseEvent *event);
+	//鼠标移动_转换后的坐标加上移动偏移量
+	void mouseMoveEvent(QMouseEvent *event);
 
 private:
 	Ui::Widget *ui;
+	QPoint point;
+	QPoint point1;
 };
 #endif // WIDGET_H
