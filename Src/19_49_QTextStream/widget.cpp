0764f0095f290ad0df8546466f6de0f2fc2a02aa
Fowindy <47002339@qq.com>
2020-11-23 12:21:28 +0800

    [技巧]:判断的时候true放在前面可以防止赋值误操作

--- Src/19_49_QTextStream/widget.cpp
+++ Src/19_49_QTextStream/widget.cpp
@@ -19,4 +19,14 @@ Widget::~Widget()
 //QTextStream:写文件
 void Widget::writeData()
 {
+	//创建文件对象
+	QFile file("./Demo.txt");
+	//打开文件_只写方式
+	bool isOpen = file.open(QIODevice::WriteOnly);
+	//判断读取
+	if (true == isOpen)	//[技巧]:判断的时候true放在前面可以防止赋值误操作
+	{
+	}
+	//创建流对象
+	QTextStream textStream(&file);
 }
