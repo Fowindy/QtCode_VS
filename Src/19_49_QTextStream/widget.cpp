7742e2e7b80063b7c8eadff3ae95c0851206284f
Fowindy <47002339@qq.com>
2020-11-23 12:13:33 +0800

    19_49_QTextStream_项目创建

---
+++ Src/19_49_QTextStream/widget.cpp
@@ -0,0 +1,15 @@
+﻿#pragma execution_character_set("utf-8")
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+	: QWidget(parent)
+	, ui(new Ui::Widget)
+{
+	ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+	delete ui;
+}
