2df331266f6f0cb4204b18a95f62b3eb1c708947
Fowindy <47002339@qq.com>
2020-11-23 22:42:39 +0800

    QBuffer_写文件

--- Src/20_50_QBuffer/widget.cpp
+++ Src/20_50_QBuffer/widget.cpp
@@ -1,12 +1,24 @@
 ﻿#pragma execution_character_set("utf-8")
 #include "widget.h"
 #include "ui_widget.h"
+#include <QBuffer>
+#include <QDebug>
 
 Widget::Widget(QWidget *parent)
 	: QWidget(parent)
 	, ui(new Ui::Widget)
 {
 	ui->setupUi(this);
+	//创建内存文件对象
+	QBuffer memFile;
+	memFile.open(QIODevice::WriteOnly);
+
+	memFile.write("11111111\n");
+	memFile.write("aaaaaaa");
+
+	memFile.close();
+	qDebug() << memFile.buffer();
+	//11111111\naaaaaaa
 }
 
 Widget::~Widget()
