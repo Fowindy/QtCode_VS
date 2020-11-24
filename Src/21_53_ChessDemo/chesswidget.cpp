7c30739f4a323e492c3840d98e461251eaac5a55
Fowindy <47002339@qq.com>
2020-11-24 11:37:43 +0800

    [不推荐]:指定位置和大小画图,缺点:固定,不会随窗口拉伸变化

--- Src/21_53_ChessDemo/chesswidget.cpp
+++ Src/21_53_ChessDemo/chesswidget.cpp
@@ -1,6 +1,7 @@
 ﻿#pragma execution_character_set("utf-8")
 #include "chesswidget.h"
 #include <QPainter>
+#include <QPixmap>
 
 ChessWidget::ChessWidget(QWidget *parent)
 	: QWidget(parent)
@@ -24,5 +25,9 @@ void ChessWidget::paintEvent(QPaintEvent *)
 
 	//创建画家对象_指定窗口为绘图设备
 	QPainter painter(this);
+
+	//画家画背景图
+	//[不推荐]:指定位置和大小画图,缺点:固定,不会随窗口拉伸变化
+	painter.drawPixmap(50, 50, 200, 200, QPixmap(":/Image/bk.jpg"));
 }
 
