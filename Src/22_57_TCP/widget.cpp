333b8b7a390a538a87dcad769d55a1983d8ff5da
Fowindy <47002339@qq.com>
2020-12-05 11:09:35 +0800

    *监听

--- Src/22_57_TCP/widget.cpp
+++ Src/22_57_TCP/widget.cpp
@@ -10,6 +10,9 @@ Widget::Widget(QWidget *parent)
 	//因为用指针初始化时动态分配空间
 	tcpServer = new QTcpServer(this);//指定父对象,自动回收内存空间,不指定也可以
 	tcpSocket = new QTcpSocket();
+
+	//*监听
+	tcpServer->listen()
 }
 
 Widget::~Widget()
