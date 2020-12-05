f5e1227d23aee4f3a3d70966caf6494c310780a5
Fowindy <47002339@qq.com>
2020-12-05 11:03:34 +0800

    指针和变量:用指针要动态分配空间,用变量要去地址符

--- Src/22_57_TCP/widget.h
+++ Src/22_57_TCP/widget.h
@@ -2,6 +2,8 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTcpServer> //监听套接字
+#include <QTcpSocket> //通信套接字
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,5 +19,8 @@ public:
 
 private:
 	Ui::Widget *ui;
+	QTcpServer *tcpServer;	//监听套接字
+	QTcpSocket *tcpSocket;	//通信套接字
+	//指针和变量:用指针要动态分配空间,用变量要去地址符
 };
 #endif // WIDGET_H
