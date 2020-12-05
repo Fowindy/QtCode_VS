9fb52d3b8e9f1fd9eaa219fb28ed7d94b3fac486
Fowindy <47002339@qq.com>
2020-12-05 10:57:14 +0800

    项目属性加network

--- Src/22_57_TCP/ui_widget.h
+++ Src/22_57_TCP/ui_widget.h
@@ -11,6 +11,10 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -18,12 +22,45 @@ QT_BEGIN_NAMESPACE
 class Ui_Widget
 {
 public:
+    QGridLayout *gridLayout;
+    QTextEdit *textEditRead;
+    QTextEdit *textEditWrite;
+    QPushButton *btnSend;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *btnClose;
 
     void setupUi(QWidget *Widget)
     {
         if (Widget->objectName().isEmpty())
             Widget->setObjectName(QString::fromUtf8("Widget"));
         Widget->resize(800, 600);
+        gridLayout = new QGridLayout(Widget);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        textEditRead = new QTextEdit(Widget);
+        textEditRead->setObjectName(QString::fromUtf8("textEditRead"));
+        textEditRead->setReadOnly(true);
+
+        gridLayout->addWidget(textEditRead, 0, 0, 1, 3);
+
+        textEditWrite = new QTextEdit(Widget);
+        textEditWrite->setObjectName(QString::fromUtf8("textEditWrite"));
+
+        gridLayout->addWidget(textEditWrite, 1, 0, 1, 3);
+
+        btnSend = new QPushButton(Widget);
+        btnSend->setObjectName(QString::fromUtf8("btnSend"));
+
+        gridLayout->addWidget(btnSend, 2, 0, 1, 1);
+
+        horizontalSpacer = new QSpacerItem(617, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout->addItem(horizontalSpacer, 2, 1, 1, 1);
+
+        btnClose = new QPushButton(Widget);
+        btnClose->setObjectName(QString::fromUtf8("btnClose"));
+
+        gridLayout->addWidget(btnClose, 2, 2, 1, 1);
+
 
         retranslateUi(Widget);
 
@@ -32,7 +69,9 @@ public:
 
     void retranslateUi(QWidget *Widget)
     {
-        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Server_\346\234\215\345\212\241\347\253\257", nullptr));
+        btnSend->setText(QCoreApplication::translate("Widget", "\345\217\221\351\200\201", nullptr));
+        btnClose->setText(QCoreApplication::translate("Widget", "\345\205\263\351\227\255", nullptr));
     } // retranslateUi
 
 };
