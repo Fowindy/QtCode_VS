80be35a99d1fe4016dd4a3cd90a83f2b967650bc
Fowindy <47002339@qq.com>
2020-12-05 12:26:17 +0800

    初始化时tcpSocket置空,防止野指针异常

--- Src/22_57_TCP/widget.cpp
+++ Src/22_57_TCP/widget.cpp
@@ -8,6 +8,8 @@ Widget::Widget(QWidget *parent)
 	, ui(new Ui::Widget)
 {
 	ui->setupUi(this);
+	//初始化时tcpSocket置空,防止野指针异常
+	tcpSocket = NULL;
 	//因为用指针初始化时动态分配空间
 	tcpServer = new QTcpServer(this);//指定父对象,自动回收内存空间,不指定也可以
 	//setWindowTitle设置服务器窗口的名字
