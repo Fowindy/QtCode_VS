9fc3e44d4c7b7b6fdebae873b03abbf3cd9a40d8
Fowindy <47002339@qq.com>
2020-12-05 12:18:09 +0800

    setWindowTitle设置服务器窗口的名字

--- Src/22_57_TCP/widget.cpp
+++ Src/22_57_TCP/widget.cpp
@@ -10,7 +10,8 @@ Widget::Widget(QWidget *parent)
 	ui->setupUi(this);
 	//因为用指针初始化时动态分配空间
 	tcpServer = new QTcpServer(this);//指定父对象,自动回收内存空间,不指定也可以
-
+	//setWindowTitle设置服务器窗口的名字
+	setWindowTitle("服务端:8888");
 	//*监听
 	//*QHostAddress::Any---绑定当前网卡所有ip(默认值)
 	tcpServer->listen(QHostAddress::Any, 8888);//指定端口:8888(默认值为:0)
