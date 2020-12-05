7b5f4521a8fae2abe76d5cfeefd3e11ee390f11f
Fowindy <47002339@qq.com>
2020-12-05 21:53:45 +0800

    this当前窗口接收_&Widget::delMessage当前窗口槽函数:接收后处理

--- Src/24_62_Udp/widget.cpp
+++ Src/24_62_Udp/widget.cpp
@@ -14,10 +14,24 @@ Widget::Widget(QWidget *parent)
 	//设置窗口标题
 	setWindowTitle("服务器端口为:8888");
 	//当对方成功发送数据过来自动触发readyRead()信号
-	connect(udpSocket, &QUdpSocket::readyRead, )
+	//this当前窗口接收_&Widget::delMessage当前窗口槽函数:接收后处理
+	connect(udpSocket, &QUdpSocket::readyRead, this, &Widget::delMessage);
 }
 
 Widget::~Widget()
 {
 	delete ui;
+}
+
+/************************************
+*@Method:    delMessage
+*@Access:    private
+*@Returns:   void
+*@Author: 	  Fowindy
+*@Created:   2020/12/05 21:51
+*@Describe:	 处理接收的消息的槽函数_因为信号无参数所以槽函数无参数
+*************************************/
+void Widget::delMessage()
+{
+
 }
