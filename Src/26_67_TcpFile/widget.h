96dc952dc2cce3b1d83745db62e7fba30ea04fb8
Fowindy <47002339@qq.com>
2020-12-07 11:00:37 +0800

    创建QTcpServer监听套接字对象和QTcpSocket通信套接字对象

--- Src/26_67_TcpFile/widget.h
+++ Src/26_67_TcpFile/widget.h
@@ -2,6 +2,8 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTcpServer>	//监听套接字
+#include <QTcpSocket>	//通信套接字
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -17,5 +19,7 @@ public:
 
 private:
 	Ui::Widget *ui;
+	QTcpServer *tcpServer;	//监听套接字对象
+	QTcpSocket *tcpSocket;	//通信套接字对象
 };
 #endif // WIDGET_H
