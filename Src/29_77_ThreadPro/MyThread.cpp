55b3e9b55e5afc3c7c275b5627de24f7328ea8a7
Fowindy <47002339@qq.com>
2021-01-12 14:44:45 +0800

    线程处理函数myTimeOut_实现

--- Src/29_77_ThreadPro/MyThread.cpp
+++ Src/29_77_ThreadPro/MyThread.cpp
@@ -9,6 +9,14 @@ MyThread::~MyThread()
 {
 }
 
+/************************************
+*@Method:    myTimeOut
+*@Access:    public
+*@Returns:   void
+*@Author: 	 Fowindy
+*@Created:   2021/01/12 14:44
+*@Describe:	 线程处理函数myTimeOut_实现
+*************************************/
 void MyThread::myTimeOut()
 {
 
