db0c3b339e8653dbb1211e5c523eb30cf473afb8
Fowindy <47002339@qq.com>
2021-01-12 10:59:25 +0800

    QThread的虚函数_run线程处理函数_不能直接调用,通过start()间接调用

---
+++ Src/28_74_QThread/MyThread.cpp
@@ -0,0 +1,15 @@
+﻿#include "MyThread.h"
+
+MyThread::MyThread(QObject *parent)
+	: QThread(parent)
+{
+}
+
+MyThread::~MyThread()
+{
+}
+
+void MyThread::run()
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
---
+++ Src/28_74_QThread/MyThread.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <QThread>
+
+class MyThread : public QThread
+{
+	Q_OBJECT
+
+public:
+	MyThread(QObject *parent);
+	~MyThread();
+protected:
+	//QThread的虚函数_run线程处理函数_不能直接调用,通过start()间接调用
+	void run();
+};
