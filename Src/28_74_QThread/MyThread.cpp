d2a3c65a5389bb0c69b27097c5759de82153f94e
Fowindy <47002339@qq.com>
2021-01-12 11:06:32 +0800

    [New]sleep:秒_msleep:毫秒_usleep:微秒

--- Src/28_74_QThread/MyThread.cpp
+++ Src/28_74_QThread/MyThread.cpp
@@ -9,7 +9,18 @@ MyThread::~MyThread()
 {
 }
 
+/************************************
+*@Method:    run
+*@Access:    protected
+*@Returns:   void
+*@Author: 	 Fowindy
+*@Created:   2021/01/12 11:01
+*@Describe:	 run线程处理函数
+*************************************/
 void MyThread::run()
 {
-	throw std::logic_error("The method or operation is not implemented.");
+	//throw std::logic_error("The method or operation is not implemented.");
+	//很复杂的数据处理需要耗时5秒
+	//sleep:秒_msleep:毫秒_usleep:微秒
+	sleep(5);
 }
